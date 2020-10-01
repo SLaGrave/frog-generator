c618b67e696b38b77c4bbfd4692ec32107b59264
Sam LaGrave <srl0011@uah.edu>
2020-10-01 18:07:19 -0500

    Add base class to the project. - Base class has extremely basic GUI skeleton.

---
+++ FrogGenerator/src/FrogGenerator.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class FrogGenerator {
+    public static void main(String[] args) {
+        System.out.println("Hello Froggie World!");
+
+        JFrame f = new JFrame("FrogGenerator");
+        f.setSize(500, 500);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        f.setVisible(true);
+
+    }
+}
